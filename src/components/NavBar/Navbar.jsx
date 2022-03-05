a127910864ac58cd50b3da92bff4bbaa422bf712
Pedro Machado <phvieiras@hotmail.com>
2022-03-05 01:37:56 -0300

    add link to logo

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { ReactComponent as Logo } from '../../assets/shared/logo.svg';
 import NavbarItems from './NavbarItems'
+import { NavLink } from 'react-router-dom'
 
 function Navbar() {
     return (
         <>
             <nav className="navbar">
-                <Logo className="navbar__logo" />
+                <NavLink className='navbar__logobox' to='/'>
+                    <Logo className="navbar__logo" />
+                </NavLink>
                 <NavbarItems />
             </nav>
         </>
